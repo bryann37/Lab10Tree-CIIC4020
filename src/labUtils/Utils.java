aada9c6f129328ca14c0c331a40f1c2c23a4ab02
4020b2 <bryan.bonilla1@upr.edu>
2018-04-23 12:52:34 -0400

    FirstCommit

---
+++ src/labUtils/Utils.java
@@ -0,0 +1,45 @@
+package labUtils;
+
+import treeClasses.LinkedBinaryTree;
+import treeClasses.LinkedTree;
+import treeInterfaces.Position;
+import treeInterfaces.Tree;
+
+public class Utils {
+	public static <E> void displayTree(String msg, Tree<E> t) { 
+		System.out.println("\n\n" + msg); 
+		t.display(); 
+	}
+
+	public static <E> void displayTreeElements(String msg, Tree<E> t) {
+		System.out.println("\n\n" + msg); 
+		for (E element : t)
+			System.out.println(element); 
+		
+	}
+	
+	public static LinkedTree<Integer> buildExampleTreeAsLinkedTree() { 
+		LinkedTree<Integer> t = new LinkedTree<>(); 
+		// ADD CODE AS SPECIFIED IN EXERCISE 2
+		t.addRoot(4);
+		Position<E> parent();
+		t.addChild(null, 9);
+//		t.addChild(null, 7);
+//		t.addChild(null, 10);
+//		t.addChild(null, 20);
+//		
+		
+		
+		return t; 
+	}
+	
+	public static LinkedBinaryTree<Integer> buildExampleTreeAsLinkedBinaryTree() { 
+		LinkedBinaryTree<Integer> t = new LinkedBinaryTree<>(); 
+		// ADD CODE AS SPECIFIED IN EXERCISE 2
+		
+		
+		return t; 
+	}
+
+
+}
